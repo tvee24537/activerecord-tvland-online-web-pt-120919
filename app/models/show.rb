889e4550c65630ba6b891bf89a07b66c8849da63
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-12 21:48:18 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
   belongs_to :network
   has_many :characters
-  has_many :actor, through: :characters
+  has_many :actors, through: :characters
 
   def actors_list
     array = []
