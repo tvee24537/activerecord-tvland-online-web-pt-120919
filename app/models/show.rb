34df9353420d489d1f0ebf0bc633efbcddf97052
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-12 20:55:14 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,13 @@ class Show < ActiveRecord::Base
   belongs_to :network
   has_many :characters
 
+  def actors_list
+    array = []
+    array_full_name =self.actors.map {|actor| actor.full_name}
+    array_of_shows = self.shows.map {|show| show.name}
+    show = array_full_name.concat array_of_shows
+    array << show.join(" - ")
+    array
+   
+  end
 end
